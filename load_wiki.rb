7e1d6b604244139865d1a1a2d18b7fcba1e55567
Josef Liska <josef.liska@virtualmaster.com>
2019-10-31 14:12:54 +0100

    rubocopize

--- load_wiki.rb
+++ load_wiki.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'bundler/inline'
 require 'open-uri'
@@ -13,17 +14,17 @@ html = open('https://cs.wikipedia.org/wiki/Simpsonovi_(18._%C5%99ada)').read
 
 doc = Nokogiri.HTML(html)
 
-#doc.css('.wikitable')
-#doc.css('.wikitable').size
-#doc.at('.wikitable')
-#doc.css('.wikitable').class
-#doc.at('.wikitable').class
-#doc.css('.wikitable').first.class
-#doc.at('.wikitable').css('tr').size
-#doc.at('.wikitable').css('tr.se-dil').size
+# doc.css('.wikitable')
+# doc.css('.wikitable').size
+# doc.at('.wikitable')
+# doc.css('.wikitable').class
+# doc.at('.wikitable').class
+# doc.css('.wikitable').first.class
+# doc.at('.wikitable').css('tr').size
+# doc.at('.wikitable').css('tr.se-dil').size
 
-doc.at('.wikitable').css('tr.se-dil').each{|row| puts row.at('td:nth(2)').text}
+doc.at('.wikitable').css('tr.se-dil').each do |row|
+  puts row.at('td:nth(2)').text
+end
 
 binding.pry
-
-
